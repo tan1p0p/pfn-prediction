6e83e1761515272ad68c2c8dae1f335f3b3d9428
hassaku <masaki.tani526@gmail.com>
2019-05-03 16:43:50 +0900

    modify train.py

--- src/train.py
+++ src/train.py
@@ -3,23 +3,19 @@ import sys
 
 import numpy as np
 import chainer
-from chainer import optimizers
-from chainer.dataset import concat_examples
-from chainer.functions import binary_accuracy
-from chainer.iterators import SerialIterator
-from chainer.optimizer_hooks import WeightDecay
 
 from modules.accuracy import joint_accuracy
 from modules.dataloader import DataLoader
 from modules.loss import mean_absolute_error_with_weight
 from modules.network import GeneratingStage, RefinementStage
 
+# TODO: Supports using GPU.
 def main():
     # load datasets
-    dataloader = DataLoader('./data/raw/hands/hand_position.json')
+    dataloader = DataLoader('./data/position.json')
     dataloader.load_data()
     train, valid, test = dataloader.split()
-    train_iter = SerialIterator(train, batch_size=4, repeat=True, shuffle=True)
+    train_iter = chainer.iterators.SerialIterator(train, batch_size=4, repeat=True, shuffle=True)
 
     # load networks
     first_stage = GeneratingStage()
@@ -34,12 +30,7 @@ def main():
         optimizers[i].setup(stage)
         for param in stage.params():
             if param.name != 'b':
-                param.update_rule.add_hook(WeightDecay(0.0001))
-
-    # config
-    n_batch = 2
-    n_epoch = 5
-    # gpu_id = 0
+                param.update_rule.add_hook(chainer.optimizer_hooks.WeightDecay(0.0001))
 
     # train
     results_train = {
@@ -55,12 +46,11 @@ def main():
         'stage2_acc': [],
         'stage3_acc': [],
     }
-    # net.to_gpu(gpu_id)
-
-    for epoch in range(n_epoch):
+    epoch_num = 5
+    for epoch in range(epoch_num):
         while True:
             train_batch = train_iter.next()
-            x_train, t_train = concat_examples(train_batch) #, gpu_id)
+            x_train, t_train = chainer.dataset.concat_examples(train_batch)
 
             # train first stage
             output_1 = networks[0](x_train)
@@ -84,33 +74,36 @@ def main():
                 present_output = current_output
 
             # validation
-            ## TODO: loss, acc to_cpu
             if train_iter.is_new_epoch:
                 with chainer.using_config('train', False), chainer.using_config('enable_backprop', False):
-                    x_valid, t_valid = chainer.dataset.concat_examples(valid) #, gpu_id)
+                    x_valid, t_valid = chainer.dataset.concat_examples(valid)
                     output_1 = networks[0](x_valid)
-                    stage1_loss = mean_absolute_error_with_weight(output_1, t_valid.astype(np.float32)).array.mean()
+                    stage1_loss = mean_absolute_error_with_weight(output_1, t_valid.astype(np.float32))
                     stage1_acc = joint_accuracy(output_1, t_valid)
 
                     output_2 = networks[1](x_valid, output_1)
-                    stage2_loss = mean_absolute_error_with_weight(output_2, t_valid.astype(np.float32)).array.mean()
+                    stage2_loss = mean_absolute_error_with_weight(output_2, t_valid.astype(np.float32))
                     stage2_acc = joint_accuracy(output_2, t_valid)
 
                     output_3 = networks[2](x_valid, output_2)
-                    stage3_loss = mean_absolute_error_with_weight(output_3, t_valid.astype(np.float32)).array.mean()
+                    stage3_loss = mean_absolute_error_with_weight(output_3, t_valid.astype(np.float32))
                     stage3_acc = joint_accuracy(output_3, t_valid)
 
                 print('epoch {}: valid_stage3_loss: {:.4f}, valid_stage3_acc: {:.4f}'.format(
-                    epoch, stage3_loss, stage3_acc))
+                    epoch, stage3_loss.array.mean(), stage3_acc))
 
-                results_valid['stage1_loss'].append(stage1_loss)
-                results_valid['stage2_loss'].append(stage2_loss)
-                results_valid['stage3_loss'].append(stage3_loss)
+                results_valid['stage1_loss'].append(stage1_loss.array.mean())
+                results_valid['stage2_loss'].append(stage2_loss.array.mean())
+                results_valid['stage3_loss'].append(stage3_loss.array.mean())
                 results_valid['stage1_acc'].append(stage1_acc)
                 results_valid['stage2_acc'].append(stage2_acc)
                 results_valid['stage3_acc'].append(stage3_acc)
 
                 break
+
+    chainer.serializers.save_npz('./model/stage1.npz', networks[0])
+    chainer.serializers.save_npz('./model/stage2.npz', networks[1])
+    chainer.serializers.save_npz('./model/stage3.npz', networks[2])
 
 if __name__ == "__main__":
     main()
